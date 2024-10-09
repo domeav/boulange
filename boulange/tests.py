2557535f786c333e807e3799f6c1bc6f0ea2d2dc
Dominique Eav <dominique.eav@blue-mind.net>
2024-10-09 17:00:24 +0200

    Empty testing

--- boulange/tests.py
+++ boulange/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ViewTests(TestCase):
+    def test_recipes(self):
+        response = self.client.get(reverse("recipes"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Semi-complet lin (2 kg)")
+    def test_orders(self):
+        response = self.client.get(reverse("orders"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Commandes")
+    def test_actions(self):
+        response = self.client.get(reverse("actions", kwargs={'year': 2024, 'month': 10, 'day': 10}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Actions pour le 10 octobre 2024")
